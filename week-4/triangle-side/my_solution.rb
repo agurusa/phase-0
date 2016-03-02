279c7129a164a726f58eb84ce471690efba513d4
Aarthi Gurusami <agurusa@gmail.com>
2016-03-02 10:54:36 -0800

    Finish triangle challenge

--- week-4/triangle-side/my_solution.rb
+++ week-4/triangle-side/my_solution.rb
@@ -4,5 +4,17 @@
 # Your Solution Below
 
 def valid_triangle?(a, b, c)
-  # Your code goes here!
+  array = [a, b, c]
+  array.sort!
+  if a==0 || b ==0 || c == 0
+    p false
+  elsif a == b && b == c
+    p true
+  elsif array[2]**2 == (array[1]**2) + (array[0]**2)
+    p true
+  elsif a + b >c && b + c > a && c + a > b
+    p true
+  else
+    p false
+  end
 end
