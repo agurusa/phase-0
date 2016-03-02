25258e3951b929d96c1d86a81641f6b51a02d771
Aarthi Gurusami <agurusa@gmail.com>
2016-03-02 10:22:57 -0800

    Calculate grade code

--- week-4/calculate-grade/my_solution.rb
+++ week-4/calculate-grade/my_solution.rb
@@ -4,3 +4,18 @@
 
 
 # Your Solution Below
+
+
+def get_grade(average)
+  if average < 60
+  p "F"
+  elsif average < 70 && average >= 60
+  p "D"
+  elsif average < 80 && average >= 70
+  p "C"
+  elsif average < 90 && average >= 80
+  p "B"
+  else average < 101 && average >= 90
+  p "A"
+  end
+end
