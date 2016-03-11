b57020ff77ec925232a4a3cea62719bb09ae0d1a
Aarthi Gurusami <agurusa@gmail.com>
2016-03-11 11:53:43 -0800

    Add description of methods to group project

--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -8,26 +8,18 @@ my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, 
 # pseudocode:
 # Input: array and string
 # Output:a new array
+# Steps:
 # # I want to search through each value in the array to see if it contains the second argument of my method (the string). I do not want to use a destructive method, because I want to return a new array and not modify the original.
-# To check whether a particular item is included in the array, I could use the boolean operator .include? But i have to include a range.
-# To select and return a new array, I can use the method .select
-# reject{|item|block} returns a new array with elements from the first array where the conditions inside the block are not met
-# select{|item|block} returns a new array with elements from the first array where the conditions inside the block are true
-#I think cover seems like the best option, but I have to figure out how to make the it evaluate to true.
-#I want it to return true if a character in a word in the array == letter
+# Make a blank array.
+# Iterate over the input array and check if the thing to find is in the word. as the word in the input array.
+  # Be sure to make everything in the array lowercase, so it'll be the same as the thing that i'm looking for, regardless of case.
+  #iterate over each letter in the word. Maybe you can do this by calling on the position of the letter in the word.
+# IF the word has the the thing I was trying to find, I want to put it in a new array.
+# ELSE do nothing.
+# return the new array with all the words that had the letter in it.
 
-# Person 1's solution
-def my_array_finding_method(i_want_pets, thing_to_find)
-  new_array = []
-  i_want_pets.each do |word|
-    range = word.to_s.downcase[0..-1]
-    if range.include?(thing_to_find)
-    new_array << word
-    end
-  end
-  return new_array
-end
 
+#initial solution
 # def my_array_finding_method(i_want_pets, thing_to_find)
 #   new_array = [] #I want to add all the strings I collect into this blank array
 #   i_want_pets.each do |word| #for each word in the original array
@@ -41,15 +33,28 @@ end
 #   p new_array #returns and prints new_array
 # end
 
+# Person 1's solution (refactored)
+def my_array_finding_method(i_want_pets, thing_to_find)
+  new_array = []
+  i_want_pets.each do |word|
+    range = word.to_s.downcase[0..-1]
+    if range.include?(thing_to_find)
+    new_array << word
+    end
+  end
+  return new_array
+end
+
 # psuedocode:
 # Input:hash and integer
 # Output:array
-# I need to find a way to go over the values in the hash. if the value == integer, i need to return the key.
+# I need to find a way to go over the values in the hash. if the value is the same as the input integer, i need to return the key.
 # I then need to put that key in a new array.
-#has_value?(value) is a boolean that will tell me if a value exists.
-# .key(value) will returns the first key of a given value. If that value does not exist, it returns nill.
-# select returns a new hash consisting of entries for which the block returns true
-#keys returns an array of the keys
+# 1. iterate over the hash.
+# 2. Compare the value in the hash to the thing to find.
+# 3. IF they are equal, store the key to that hash in a new array.
+# 4. ELSE do nothing.
+# 5. Return that new array.
 
 
 def my_hash_finding_method(my_family_pets_ages, thing_to_find)
@@ -57,9 +62,12 @@ def my_hash_finding_method(my_family_pets_ages, thing_to_find)
 end
 
 # Identify and describe the Ruby method(s) you implemented.
-#
-#
-#
+## include? checks whether a particular item is included in the array. It is just a boolean operator, so you have to decide what you want your program to do if it returns true or false. You have to include a range for this method to iterate over.
+## << is the shovel method. If you write a<<b, the value in b is "shovelled" over to array a. It becomes the last item in that array. This is a destructive method, so be careful if you're trying to do something non-destructively!
+## select returns a new hash consisting of entries for which the block returns true. There is a boolean operation that is its equivalent, but returning a hash made more sense to this particular problem.
+##keys returns an array of the keys in the hash.
+
+
 
 # Person 2
 # def my_array_modification_method!(source, thing_to_modify)
