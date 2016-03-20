991b9927038b44798e10ced7e63cf0b3f9fb4fee
Aarthi Gurusami <agurusa@gmail.com>
2016-03-20 14:21:01 -0700

    Finish guessing game

--- week-6/guessing-game/my_solution.rb
+++ week-6/guessing-game/my_solution.rb
@@ -27,6 +27,36 @@
 
 # Initial Solution
 
+# class GuessingGame
+#   def initialize(answer)
+#     @answer = answer
+#   end
+
+#   def guess(integer)
+#     @integer = integer
+#     if @integer > @answer
+#       return :high
+#     elsif @integer< @answer
+#       return :low
+#     else @integer == @answer
+#       return :correct
+#     end
+#     solved?
+#   end
+
+#   def solved?
+#     if @integer == @answer
+#       return true
+#     else
+#       return false
+#     end
+#   end
+# end
+
+
+
+# Refactored Solution
+
 class GuessingGame
   def initialize(answer)
     @answer = answer
@@ -34,27 +64,32 @@ class GuessingGame
 
   def guess(integer)
     @integer = integer
-    if @integer > @answer
-      return :high
-    elsif @integer< @answer
-      return :low
-    else @integer == @answer
-      return :correct
-    end
-    solved?
+    @integer > @answer ? (:high) : ( @integer < @answer ? (:low) : (:correct))
   end
 
   def solved?
-    if @integer == @answer
-      return true
-    else
-      return false
-    end
+    @integer == @answer ? (true) : (false)
   end
-  # Make sure you define the other required methods, too
+
 end
 
+# Reflection
+
+# How do instance variables and methods represent the characteristics and behaviors (actions) of a real-world object?
+
+# Instance variables are like the nouns of the world, and methods are like the verb acting on that noun.
+
+# When should you use instance variables? What do they do for you?
+
+# You should use instance variables when you're going to access those variables every time you call an instance of the class. They allow you to use variables throughout your class code. They also allow you to set different values for those variables for different objects, even if they are of the same Class. But from outside the object, they can't be altered.
+
+# Explain how to use flow control. Did you have any trouble using it in this challenge? If so, what did you struggle with?
+
+# Flow control is used when your program has different conditionals attached to the outcome. The program will check the first statement, and if that conditional is not filled (if it doesn't evaluate to true), it will move on to the next conditional statement. I didn't have any trouble using it in this challenge. I would say the hardest part about flow control was in the refactored section, where I introduced ternary operators. When doing this, I had to understand the parenthetical operations, and why they made the code more readable. This all has to do with flow control, and being able to read each nested set of parentheses as a new part of the flow.
 
 
-# Refactored Solution
+# Why do you think this code requires you to return symbols? What are the benefits of using symbols?
 
+# Symbols save memory and time by looking at the symbol as an integer. A symbol can only be that symbol, and not a string of characters. Strings are also immutable, meaning that you can't change them at runtime. For a constant value, like true, false, high, low, and correct, these will not change in the way that we typically want to change and manipulate strings. This was a really good link to help me understand symbols better:
+
+# http://www.troubleshooters.com/codecorn/ruby/symbols.htm#_What_can_symbols_do_for_you
