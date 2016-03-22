44cc805c14467508696749ee8d41971819be6c45
Aarthi Gurusami <agurusa@gmail.com>
2016-03-22 09:17:54 -0700

    Finish gps

--- week-7/gps6/my_solution.rb
+++ week-7/gps6/my_solution.rb
@@ -1,61 +1,72 @@
-# Virus Predictor
-
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+require_relative(STATE_DATA)
 
 # EXPLANATION OF require_relative
-#
-#
-require_relative 'state_data'
+#require_relative: Direct to a file in the same directory.
+#require: Loads in any file in the application
 
 class VirusPredictor
-
+#Initializing and creating instance variables.
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
+#Calls two other methods in the class
 
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths#(@population_density, @population, @state)
+    speed_of_spread#(@population_density, @state)
   end
 
   private
-
-  def predicted_deaths(population_density, population, state)
+#Compares population density to a number. Depending on the result of that comparison, the number of deaths will change.
+#Floor method rounds down to the nearest integer.
+#Prints a string
+  def predicted_deaths#(population_density, population, state)
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+
+    x = 1
+
+    number_of_deaths = (@population * x).floor
+    case @population_density
+
+    when (150...200)
+     x = 0.3
+    when (100...150)
+      x = 0.2
+    when (50...100)
+      x = 0.1
+    when (0...50)
+      x= 0.05
     else
-      number_of_deaths = (@population * 0.05).floor
+      x = 0.4
     end
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
-
-  def speed_of_spread(population_density, state) #in months
+#Intializes a counter. Compares pop density to a number, determines the speed of the spread.
+#Prints a statement
+  def speed_of_spread#(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
+    # speed = 0.0
+    case @population_density
+      when  (151...200)
+        speed = 1
 
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
-    else
-      speed += 2.5
+      when  (100...150)
+        speed = 1.5
+
+      when  (50...100)
+        speed = 2
+
+      when (0..50)
+        speed = 2.5
+
+      else
+        speed = 0.5
+
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
@@ -69,7 +80,17 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
+def report(hash)
+  hash.each do |state, info|
+    states= VirusPredictor.new(state, info[:population_density], info[:population])
+    states.virus_effects
+  end
+end
+report(STATE_DATA)
 
+
+
+=begin
 alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
 alabama.virus_effects
 
@@ -81,7 +102,24 @@ california.virus_effects
 
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
-
+=end
 
 #=======================================================================
 # Reflection Section
+=begin
+What are the differences between the two different hash syntaxes shown in the state_data file?
+The outside hash is made up of a key that is a string, and the value is another hash. The inside hash is made up of a key that is a symbol and a value that is an integer.
+
+What does require_relative do? How is it different from require?
+require_relative allows you to use a local path directory, and is generally faster than just using require. Require can call on files from anywhere.
+
+What are some ways to iterate through a hash?
+We iterated through our hash using .each.
+
+When refactoring virus_effects, what stood out to you about the variables, if anything?
+We noticed that they were continually calling instance variables, which can be accessed throughout the class, and do not have to be made into arguments for each method.
+
+What concept did you most solidify in this challenge?
+I really solidified iterating through hashes, and creating a new instance of a class through this iteration.
+
+=end
