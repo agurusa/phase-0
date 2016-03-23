f9853bbb9635b896065f20e73818e71aff5b059b
Aarthi Gurusami <agurusa@gmail.com>
2016-03-23 11:51:54 -0700

    Work on game code

--- week-7/game.js
+++ week-7/game.js
@@ -10,6 +10,11 @@ var puppy = {
 var interval;
 
 function lifetime(){
+  puppy.full = 15;
+  puppy.bathroom =15;
+  puppy.happiness = 100;
+  puppy.intelligence = 100;
+  puppy.health = 100;
   overTime();
 }
 
@@ -44,11 +49,9 @@ function decreaseProps(){
 
   if (puppy.happiness <= 0){
     runAway();
-    pause();
   }
   else if (puppy.health <= 0){
     death();
-    pause();
   }
   else if (puppy.full <= 0 || puppy.bathroom <= 0){
     puppy.happiness -= 5;
@@ -74,12 +77,14 @@ function instructions(){
 
 function runAway(){
   var message = document.getElementById('message');
-  message.textContent = "Your puppy ran away! Game over."
+  message.textContent = "Your puppy ran away! Game over.";
+  pause();
 }
 
 function death(){
   var message = document.getElementById('message');
-  message.textContent = "Your puppy's health was too low! It died :("
+  message.textContent = "Your puppy's health was too low! It died :(";
+  pause();
 }
 
 
@@ -119,25 +124,25 @@ function vet(){
 var otherPuppy = ["good mood", "bad mood"];
 
 function socialize(){
-  var message = document.getElementById('message');
-  var feedback = document.getElementById('feedback');
-  message.textContent = "So you want to play with neighborhood dog Gus?"
+  var message = document.getElementById('socialize');
+  var feedback = document.getElementById('socialize-feedback');
+  message.textContent = "So you want to play with neighborhood dog Gus? Beware, sometimes he's not in the best of moods..."
   var index = (Math.round(Math.random()));
   var puppyMood = otherPuppy[index];
+
   if (puppyMood === "bad mood"){
     puppy.happiness -=3;
     puppy.health -=3;
     feedback.textContent = "Gus was in the worst mood ever! He attacked me!!";
     feedback.style.color = "#ff3399";
-    if (puppy.happiness <=0){
-    runAway();
-    }
   }
+
   else{
     puppy.happiness +=7;
     feedback.textContent = "Gus was in such a good mood. We played all day.";
     feedback.style.color = "#ff3399";
   }
+
 }
 
 function saySomething(){
@@ -147,51 +152,22 @@ function saySomething(){
     message.textContent = "Feed me, you fool!";
     message.style.color = "#ff3399";
   }
+
   else if (puppy.bathroom <=10){
     message.textContent = "Walk me, or I will pee in your favorite shoe."
     message.style.color = "#ff3399";
   }
+
   else if (puppy.happiness <=10){
     message.textContent = "I'm going to run away soon...better try and stop me...";
     message.style.color = "#ff3399";
   }
-  // if (puppy.intelligence <=10){
-  //   message.textContent = "Sorry, I'm too stupid to listen to you when you yell at me.";
-  // }
-  //     else
+
   else{
     message.textContent = "I love you, human.";
     message.style.color = "#339966";
   }
 }
-
-// function newGame(){
-//   var elem = document.getElementById("full");
-//   elem.style.width = ((puppy.full).toString()) + "px";
-//   var elem = document.getElementById("bathroom");
-//   elem.style.width = ((puppy.bathroom).toString()) + "px";
-//   var elem = document.getElementById("happiness");
-//   elem.style.width = ((puppy.happiness).toString()) + "px"
-//   var elem = document.getElementById("intelligence");
-//   elem.style.width = ((puppy.intelligence).toString()) + "px"
-//   var elem = document.getElementById("health");
-//   elem.style.width = ((puppy.health).toString()) + "px"
-// }
-
-// function start(){
-//   var thing = document.getElementById("full");
-//   var pos = 10;
-//   var id = setInterval(frame, 5);
-//   function frame() {
-//     if (pos == 100) {
-//       clearInterval(id);
-//     }
-//     else {
-//       pos++;
-//       thing.style.width = pos + 'px';
-//     }
-//   }
-// }
 
 
 // Refactored Code
